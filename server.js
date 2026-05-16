4c2a3f381a18760bba27f8ba7d0bd2d6730f501d
vigneshsekar2003s <vigneshsekar2003s@gmail.com>
2026-05-16 22:29:34 +0530

    Fix Backend URL

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+       origin:
+      "https://crm-frontend-alpha-beige.vercel.app",
+      credentials: true,
+  })
+);
 
 
 mongoose
