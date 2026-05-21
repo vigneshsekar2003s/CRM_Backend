9bc86d90aeed3e6f03b75344592ad0ed72a79191
vigneshsekar2003s <vigneshsekar2003s@gmail.com>
2026-05-21 21:53:36 +0530

    Fixed frontend api

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -32,6 +38,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0",() => {
+app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
